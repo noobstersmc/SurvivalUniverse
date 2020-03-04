ec33696830d9a20c607ec30087a2514251fc88cd
InfinityZ25 <henixceo@gmail.com>
2020-03-03 23:40:06 -0500

    Initial Commit

---
+++ src/main/java/me/infinityz/SurvivalUniverse.java
@@ -0,0 +1,21 @@
+package me.infinityz;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * SurvivalUniverse
+ */
+public class SurvivalUniverse extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        
+    }
+
+    @Override
+    public void onDisable(){
+        
+    }
+
+    
+}
