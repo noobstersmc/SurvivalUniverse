26e6ee9c2f444a20696fd6d0e5200c981a345c4e
InfinityZ25 <henixceo@gmail.com>
2020-04-21 11:30:48 -0400

    Removed unnecesary code from branch merge

--- src/main/java/me/infinityz/commands/ChunkCommands.java
+++ src/main/java/me/infinityz/commands/ChunkCommands.java
@@ -1,22 +1,11 @@
 package me.infinityz.commands;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonPrimitive;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -29,12 +18,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
-import org.bukkit.util.io.BukkitObjectInputStream;
-import org.bukkit.util.io.BukkitObjectOutputStream;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import me.infinityz.SurvivalUniverse;
 import me.infinityz.chunks.IChunk;
@@ -608,23 +591,6 @@ public class ChunkCommands implements CommandExecutor, TabCompleter {
 
                     break;
                 }
-                case "save": {
-                    //data.put(player.getUniqueId(), serializePlayerInfo(player));
-                    itemStackArrayToBase64(player.getInventory().getContents());
-                    break;
-                }
-                case "retrieve": {
-                    List<String> arrays = Arrays.asList(data.get(player.getUniqueId()));
-                    try {
-                        player.getInventory().setContents(itemStackArrayFromBase64(arrays.get(0)));
-                        player.getInventory().setArmorContents(itemStackArrayFromBase64(arrays.get(1)));
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-
-                    break;
-                }
                 default: {
                     sender.sendMessage(ChatColor.RED + "Command usage: /ally <add:remove> [Player]");
                     return false;
@@ -633,207 +599,6 @@ public class ChunkCommands implements CommandExecutor, TabCompleter {
             return true;
         }
         return false;
-    }
-
-    public Map<UUID, String[]> data = new HashMap<>();
-
-    /**
-     * Converts the player inventory to a String array of Base64 strings. First
-     * string is the content and second string is the armor.
-     * 
-     * @param playerInventory to turn into an array of strings.
-     * @return Array of strings: [ main content, armor content ]
-     * @throws IllegalStateException
-     */
-    public static String[] serializePlayerInfo(Player player) {
-        JsonObject obj = new JsonObject();
-        String content = toBase64(player.getInventory());
-        String ender_chest = toBase64(player.getEnderChest());
-        String armor = itemStackArrayToBase64(player.getInventory().getArmorContents());
-
-        obj.add("uuid", new JsonPrimitive(player.getUniqueId().toString()));
-        obj.add("last_known_name", new JsonPrimitive(player.getName()));
-        /**
-         * Serialize inventory and enderchest inventory and add them to the json object
-         */
-        JsonObject jsonInventory = new JsonObject();
-        jsonInventory.add("held_item_slot", new JsonPrimitive(player.getInventory().getHeldItemSlot()));
-        jsonInventory.add("contents", new JsonPrimitive(content));
-        jsonInventory.add("armor_contents", new JsonPrimitive(armor));
-        jsonInventory.add("ender_chest", new JsonPrimitive(ender_chest));
-        obj.add("inventory", jsonInventory);
-        /** Turn active potion effects into a json object */
-        JsonObject active_effects = new JsonObject();
-        player.getActivePotionEffects().stream().forEach(potion -> {
-            JsonObject potionJson = new JsonObject();
-            potionJson.addProperty("amplifier", potion.getAmplifier());
-            potionJson.addProperty("duration", potion.getDuration());
-            active_effects.add(potion.getType().getName(), potionJson);
-        });
-        obj.add("potion_effects", active_effects);
-        /** Get health, absoption, food, saturation, level, and add them to the json */
-        JsonObject player_status = new JsonObject();
-        player_status.add("health", new JsonPrimitive(player.getHealth()));
-        player_status.add("absorption_hearts", new JsonPrimitive(player.getAbsorptionAmount()));
-        player_status.add("food_level", new JsonPrimitive(player.getFoodLevel()));
-        player_status.add("saturation", new JsonPrimitive(player.getSaturation()));
-        player_status.add("exp_level", new JsonPrimitive(player.getLevel()));
-        player_status.add("exp_percentage", new JsonPrimitive(player.getExp()));
-        obj.add("status", player_status);
-        // Use this to print a pretty Gson
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        //Write JSON file
-        try (FileWriter file = new FileWriter("./" + player.getUniqueId().toString()+".json")) {
- 
-            file.write(gson.toJson(obj));
-            file.flush();
- 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        System.out.println("\n" + gson.toJson(obj) + "\n");
-        // Change this
-        return new String[] { content, armor };
-
-    }
-
-    public static String[] playerInventoryToBase64(PlayerInventory playerInventory) throws IllegalStateException {
-        // get the main content part, this doesn't return the armor
-
-        String content = toBase64(playerInventory);
-        String armor = itemStackArrayToBase64(playerInventory.getArmorContents());
-
-        return new String[] { content, armor };
-    }
-
-    /**
-     * 
-     * A method to serialize an {@link ItemStack} array to Base64 String.
-     * 
-     * <p />
-     * 
-     * Based off of {@link #toBase64(Inventory)}.
-     * 
-     * @param items to turn into a Base64 String.
-     * @return Base64 string of the items.
-     * @throws IllegalStateException
-     */
-    public static String itemStackArrayToBase64(ItemStack[] items) throws IllegalStateException {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-
-            // Write the size of the inventory
-            dataOutput.writeInt(items.length);
-
-            // Save every element in the list
-            for (int i = 0; i < items.length; i++) {
-                dataOutput.writeObject(items[i]);
-            }
-
-            // Serialize that array
-            dataOutput.close();
-            return Base64Coder.encodeLines(outputStream.toByteArray());
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to save item stacks.", e);
-        }
-    }
-
-    /**
-     * A method to serialize an inventory to Base64 string.
-     * 
-     * <p />
-     * 
-     * Special thanks to Comphenix in the Bukkit forums or also known as aadnk on
-     * GitHub.
-     * 
-     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
-     * 
-     * @param inventory to serialize
-     * @return Base64 string of the provided inventory
-     * @throws IllegalStateException
-     */
-    public static String toBase64(Inventory inventory) throws IllegalStateException {
-        try {
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            BukkitObjectOutputStream dataOutput = new BukkitObjectOutputStream(outputStream);
-
-            // Write the size of the inventory
-            dataOutput.writeInt(inventory.getSize());
-
-            // Save every element in the list
-            for (int i = 0; i < inventory.getSize(); i++) {
-                dataOutput.writeObject(inventory.getItem(i));
-            }
-
-            // Serialize that array
-            dataOutput.close();
-            return Base64Coder.encodeLines(outputStream.toByteArray());
-        } catch (Exception e) {
-            throw new IllegalStateException("Unable to save item stacks.", e);
-        }
-    }
-
-    /**
-     * 
-     * A method to get an {@link Inventory} from an encoded, Base64, string.
-     * 
-     * <p />
-     * 
-     * Special thanks to Comphenix in the Bukkit forums or also known as aadnk on
-     * GitHub.
-     * 
-     * <a href="https://gist.github.com/aadnk/8138186">Original Source</a>
-     * 
-     * @param data Base64 string of data containing an inventory.
-     * @return Inventory created from the Base64 string.
-     * @throws IOException
-     */
-    public static Inventory fromBase64(String data) throws IOException {
-        try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
-            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-            Inventory inventory = Bukkit.getServer().createInventory(null, dataInput.readInt());
-
-            // Read the serialized inventory
-            for (int i = 0; i < inventory.getSize(); i++) {
-                inventory.setItem(i, (ItemStack) dataInput.readObject());
-            }
-
-            dataInput.close();
-            return inventory;
-        } catch (ClassNotFoundException e) {
-            throw new IOException("Unable to decode class type.", e);
-        }
-    }
-
-    /**
-     * Gets an array of ItemStacks from Base64 string.
-     * 
-     * <p />
-     * 
-     * Base off of {@link #fromBase64(String)}.
-     * 
-     * @param data Base64 string to convert to ItemStack array.
-     * @return ItemStack array created from the Base64 string.
-     * @throws IOException
-     */
-    public static ItemStack[] itemStackArrayFromBase64(String data) throws IOException {
-        try {
-            ByteArrayInputStream inputStream = new ByteArrayInputStream(Base64Coder.decodeLines(data));
-            BukkitObjectInputStream dataInput = new BukkitObjectInputStream(inputStream);
-            ItemStack[] items = new ItemStack[dataInput.readInt()];
-
-            // Read the serialized inventory
-            for (int i = 0; i < items.length; i++) {
-                items[i] = (ItemStack) dataInput.readObject();
-            }
-
-            dataInput.close();
-            return items;
-        } catch (ClassNotFoundException e) {
-            throw new IOException("Unable to decode class type.", e);
-        }
     }
 
     @Override
